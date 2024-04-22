c52d22b29870d7f228ae21261b247f7afb38cd74
zhenkai.sun <zhenkai.sun@qq.com>
2024-04-22 19:29:29 +0800

    update

--- vcpkg-overlay/cpp-common/portfile.cmake
+++ vcpkg-overlay/cpp-common/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO sunzhenkai/cpp-common
     REF "${VERSION}"
-    SHA512 6fd36845e255bf94a432fb5dea24ed24b2d0dbd431979cccb5960af3e8206babee3f1c9fdfa0de3da65205f6c833ca81e2ad58696b824e622c0952cd3efbc385
+    SHA512 33fbc6e82cd313105278c04e840ff9af5da0db1d4072dc34492a7fb86ff73bf08e27e3822bfaf50f849fbcb34b4edde022b41891ddaf7ff5f559b75d3f62fedb
     HEAD_REF main
 )
 
@@ -20,5 +20,5 @@ vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-#file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
