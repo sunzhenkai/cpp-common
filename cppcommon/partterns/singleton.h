cacbf0111ff41e429b27ab5cd392495565a77e68
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-27 10:03:49 +0800

    fix singleton header

--- cppcommon/partterns/singleton.h
+++ cppcommon/partterns/singleton.h
@@ -4,20 +4,22 @@
  * @author zhenkai.sun
  * @date 2025-03-19 16:56:41
  */
+#pragma once
 #include <iostream>
 
 namespace cppcommon {
-template <class T> class Singleton {
-protected:
+template <class T>
+class Singleton {
+ protected:
   Singleton() = default;
   virtual ~Singleton() = default;
   Singleton(const Singleton &) = delete;
   Singleton &operator=(const Singleton &) = delete;
 
-public:
+ public:
   static T &Instance() {
     static T instance;
     return instance;
   }
 };
-} // namespace cppcommon
+}  // namespace cppcommon
