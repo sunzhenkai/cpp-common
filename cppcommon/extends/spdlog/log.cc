0309c0f66eff0815b7bf41e3a0559baf0cae652c
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-30 19:54:07 +0800

    mod log

---
+++ cppcommon/extends/spdlog/log.cc
@@ -0,0 +1,10 @@
+#include "log.h"
+
+#include <spdlog/logger.h>
+#include <spdlog/sinks/stdout_sinks.h>
+
+#include <memory>
+
+namespace cppcommon::extends {
+const std::shared_ptr<spdlog::logger> kConsoleLogger = spdlog::stdout_logger_mt("cppcommon_console_logger");
+}
--- cppcommon/extends/spdlog/log.h
+++ cppcommon/extends/spdlog/log.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <spdlog/logger.h>
+
+#include <memory>
+
 #include "spdlog/async.h"
 #include "spdlog/sinks/rotating_file_sink.h"
 #include "spdlog/spdlog.h"
 
 namespace cppcommon::extends {
+extern const std::shared_ptr<spdlog::logger> kConsoleLogger;
+
 #ifndef SPDLOG_LEVEL
 #ifdef ENABLE_DEBUG
 #define SPDLOG_LEVEL spdlog::level::debug
@@ -39,15 +45,38 @@ namespace cppcommon::extends {
 #endif
 
 DECLARE_LOGGER(Runtime, runtime, 1024, 5, SPDLOG_LEVEL, GENERAL_LOG_FORMAT);
-DECLARE_LOGGER(Error, error, 1024, 5, SPDLOG_LEVEL, GENERAL_LOG_FORMAT);
+DECLARE_LOGGER(Record, record, 1024, 5, SPDLOG_LEVEL, GENERAL_LOG_FORMAT);
 
+//< runtime log for normal infomation printing
 #define RUNTIME_DEBUG(format, args...) \
   SPDLOG_LOGGER_DEBUG(cppcommon::extends::RuntimeLogger::GetLogger(), format, ##args)
 #define RUNTIME_INFO(format, args...) SPDLOG_LOGGER_INFO(cppcommon::extends::RuntimeLogger::GetLogger(), format, ##args)
 #define RUNTIME_WARN(format, args...) SPDLOG_LOGGER_WARN(cppcommon::extends::RuntimeLogger::GetLogger(), format, ##args)
 #define RUNTIME_ERROR(format, args...) \
   SPDLOG_LOGGER_ERROR(cppcommon::extends::RuntimeLogger::GetLogger(), format, ##args)
+#define RUNTIME_CRITICAL(format, args...) \
+  SPDLOG_LOGGER_CRITICAL(cppcommon::extends::RuntimeLogger::GetLogger(), format, ##args)
 
-#define ERROR(format, args...) SPDLOG_LOGGER_ERROR(cppcommon::extends::ErrorLogger::GetLogger(), format, ##args)
-#define CRITICAL(format, args...) SPDLOG_LOGGER_CRITICAL(cppcommon::extends::ErrorLogger::GetLogger(), format, ##args)
+//< INFO, WARN, ERROR, CRITICAL for important log printing
+//< these logs will be sinked into stdout and record.log
+#ifndef INFO
+#define INFO(format, args...)                                                        \
+  SPDLOG_LOGGER_INFO(cppcommon::extends::RecordLogger::GetLogger(), format, ##args); \
+  cppcommon::extends::kConsoleLogger->info(format, ##args);
+#endif  // !INFO
+#ifndef WARN
+#define WARN(format, args...)                                                        \
+  SPDLOG_LOGGER_WARN(cppcommon::extends::RecordLogger::GetLogger(), format, ##args); \
+  cppcommon::extends::kConsoleLogger->warn(format, ##args);
+#endif  // !WARN
+#ifndef ERROR
+#define ERROR(format, args...)                                                        \
+  SPDLOG_LOGGER_ERROR(cppcommon::extends::RecordLogger::GetLogger(), format, ##args); \
+  cppcommon::extends::kConsoleLogger->error(format, ##args);
+#endif  // !ERROR
+#ifndef CRITICAL
+#define CRITICAL(format, args...)                                                        \
+  SPDLOG_LOGGER_CRITICAL(cppcommon::extends::RecordLogger::GetLogger(), format, ##args); \
+  cppcommon::extends::kConsoleLogger->critical(format, ##args);
+#endif  // !CRITICAL
 }  // namespace cppcommon::extends
