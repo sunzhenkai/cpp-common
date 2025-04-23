0fed8af53c3262b743b77cdbaf2227ecf48b2422
Wii <zhenkai.sun@qq.com>
2025-04-23 15:57:48 +0800

    enhance date info (#12)
    * update
    
    * update

--- tests/gtest/gtest_utils.cc
+++ tests/gtest/gtest_utils.cc
@@ -5,12 +5,12 @@
 
 TEST(Common, Weekday) {
   int64_t ts_ms = 1745377872000;
-  std::cout << cppcommon::GetWeekEnd(0, ts_ms) << std::endl;
-  ASSERT_EQ(cppcommon::GetWeekEnd(1745378196000, 8), 2);
-  ASSERT_EQ(cppcommon::GetWeekEnd(1743264000000, 8), 6);
-  ASSERT_EQ(cppcommon::GetWeekEnd(1743264000000, 8), 6);
-  ASSERT_EQ(cppcommon::GetWeekEnd(1461340800000, 8), 5);
-  ASSERT_EQ(cppcommon::GetWeekEnd(1461340800000, 0), 4);
-  ASSERT_EQ(cppcommon::GetWeekEnd(1408291199000, 0), 6);
-  ASSERT_EQ(cppcommon::GetWeekEnd(1408291199000, 0), 6);
+  std::cout << cppcommon::GetWeekDay(0, ts_ms) << std::endl;
+  ASSERT_EQ(cppcommon::GetWeekDay(1745378196000, 8), 2);
+  ASSERT_EQ(cppcommon::GetWeekDay(1743264000000, 8), 6);
+  ASSERT_EQ(cppcommon::GetWeekDay(1743264000000, 8), 6);
+  ASSERT_EQ(cppcommon::GetWeekDay(1461340800000, 8), 5);
+  ASSERT_EQ(cppcommon::GetWeekDay(1461340800000, 0), 4);
+  ASSERT_EQ(cppcommon::GetWeekDay(1408291199000, 0), 6);
+  ASSERT_EQ(cppcommon::GetWeekDay(1408291199000, 0), 6);
 }
