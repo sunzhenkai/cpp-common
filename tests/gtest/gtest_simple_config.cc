7e5bef11fefa4c3b20bc8d91a73765329b6ec141
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-29 13:33:59 +0800

    add simple config

---
+++ tests/gtest/gtest_simple_config.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+#include "cppcommon/utils/simple_config.h"
+#include "gtest/gtest.h"
+
+TEST(SimpleConfig, Base) {
+  std::string c = "a:1;b:2;pi:3.14";
+  cppcommon::SimpleConfig cfg(c, false);
+
+  std::cout << cfg.ToString() << std::endl;
+  ASSERT_EQ(1, cfg.GetInt64("a", -1));
+  ASSERT_EQ("1", cfg.Get("a", ""));
+  ASSERT_EQ("3.14", cfg.Get("pi", ""));
+  ASSERT_EQ(3.14, cfg.GetDouble("pi", 0));
+  ASSERT_EQ(0, cfg.GetDouble("nan", 0));
+  ASSERT_EQ(-1, cfg.GetInt64("nan", -1));
+}
+
+TEST(SimpleConfig, BadCase1) {
+  std::string c = "a:1;b:2;pi:3.14;c:;w:false;z::a;ac";
+  cppcommon::SimpleConfig cfg(c, false);
+
+  std::cout << cfg.ToString() << std::endl;
+  ASSERT_EQ(-1, cfg.GetInt64("c", -1));
+  ASSERT_EQ("", cfg.Get("c", "nan"));
+  ASSERT_EQ("nan", cfg.Get("nn", "nan"));
+  ASSERT_EQ(false, cfg.GetBoolean("w", true));
+
+  ASSERT_EQ(0, cfg.GetDouble("nan", 0));
+  ASSERT_EQ(-1, cfg.GetInt64("nan", -1));
+  ASSERT_EQ(":a", cfg.Get("z", ""));
+}
