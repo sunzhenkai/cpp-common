ad1b157627ef6b614e9b2e4339adefe7cb576459
zhenkai.sun <zhenkai.sun@qq.com>
2025-05-20 13:24:53 +0800

    mod rapidjson get value

--- cppcommon/extends/rapidjson/reader.h
+++ cppcommon/extends/rapidjson/reader.h
@@ -13,6 +13,50 @@
 #include "rapidjson/document.h"
 
 namespace cppcommon {
+
+template <typename T, typename Arg, typename... Rest>
+T GetRjValue(const rapidjson::Value* node, const T& dft, Arg&& arg, Rest&&... rest);
+template <typename T, std::enable_if_t<cppcommon::is_vector_v<T>, int> = 0>
+T GetRjValue(const rapidjson::Value* node, const T& dft);
+template <typename T, std::enable_if_t<!cppcommon::is_vector_v<T>, int> = 0>
+T GetRjValue(const rapidjson::Value* node, const T& dft);
+
+template <typename T, typename Arg, typename... Rest>
+T GetRjValue(const rapidjson::Value* node, const T& dft, Arg&& arg, Rest&&... rest) {
+  if (!(node) || !(node)->IsObject() || !(node)->HasMember(arg)) {
+    return dft;
+  }
+  node = &(node)->operator[](arg);
+  return GetRjValue<T>(node, dft, std::forward<Rest>(rest)...);
+}
+
+template <typename T, std::enable_if_t<!cppcommon::is_vector_v<T>, int>>
+T GetRjValue(const rapidjson::Value* node, const T& dft) {
+  if (!(node)) return dft;
+  if constexpr (std::is_same_v<T, std::string>) {
+    return (node)->IsString() ? (node)->GetString() : dft;
+  } else if constexpr (std::is_integral_v<T>) {
+    return (node)->IsInt() ? (node)->GetInt() : dft;
+  } else if constexpr (std::is_floating_point_v<T>) {
+    return (node)->IsDouble() ? (node)->GetDouble() : dft;
+  } else if constexpr (std::is_same_v<T, bool>) {
+    return (node)->IsBool() ? (node)->GetBool() : dft;
+  }
+  return dft;
+}
+
+template <typename T, std::enable_if_t<cppcommon::is_vector_v<T>, int>>
+T GetRjValue(const rapidjson::Value* node, const T& dft) {
+  if (!node || !node->IsArray()) {
+    return dft;
+  }
+  T res;
+  for (auto it = node->Begin(); it != node->End(); ++it) {
+    res.emplace_back(GetRjValue(it, VectorElementType_t<T>{}));
+  }
+  return res;
+}
+
 class JsonReader {
  public:
   explicit JsonReader(const std::string& json_str);
@@ -21,46 +65,10 @@ class JsonReader {
   template <typename T, typename... Args>
   T Get(const T& dft, Args&&... args) const {
     const rapidjson::Value* node = &doc_;
-    return GetValueImpl<T>(node, dft, std::forward<Args>(args)...);
+    return GetRjValue<T>(node, dft, std::forward<Args>(args)...);
   }
 
  private:
-  template <typename T, typename Arg, typename... Rest>
-  T GetValueImpl(const rapidjson::Value* node, const T& dft, Arg&& arg, Rest&&... rest) const {
-    if (!(node) || !(node)->IsObject() || !(node)->HasMember(arg)) {
-      return dft;
-    }
-    node = &(node)->operator[](arg);
-    return GetValueImpl<T>(node, dft, std::forward<Rest>(rest)...);
-  }
-
-  template <typename T, std::enable_if_t<!cppcommon::is_vector_v<T>, int> = 0>
-  T GetValueImpl(const rapidjson::Value* node, const T& dft) const {
-    if (!(node)) return dft;
-    if constexpr (std::is_same_v<T, std::string>) {
-      return (node)->IsString() ? (node)->GetString() : dft;
-    } else if constexpr (std::is_integral_v<T>) {
-      return (node)->IsInt() ? (node)->GetInt() : dft;
-    } else if constexpr (std::is_floating_point_v<T>) {
-      return (node)->IsDouble() ? (node)->GetDouble() : dft;
-    } else if constexpr (std::is_same_v<T, bool>) {
-      return (node)->IsBool() ? (node)->GetBool() : dft;
-    }
-    return dft;
-  }
-
-  template <typename T, std::enable_if_t<cppcommon::is_vector_v<T>, int> = 0>
-  T GetValueImpl(const rapidjson::Value* node, const T& dft) const {
-    if (!node || !node->IsArray()) {
-      return dft;
-    }
-    T res;
-    for (auto it = node->Begin(); it != node->End(); ++it) {
-      res.emplace_back(GetValueImpl(it, VectorElementType_t<T>{}));
-    }
-    return res;
-  }
-
   rapidjson::Document doc_;
 };
 
