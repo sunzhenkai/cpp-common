b8d0e9940e40758265f80fa680c4f1aa5acd2731
zhenkai.sun <zhenkai.sun@qq.com>
2025-05-22 22:50:43 +0800

    add api

---
+++ cppcommon/api.h
@@ -0,0 +1,20 @@
+/**
+ * @file api.h
+ * @brief
+ * @author zhenkai.sun
+ * @date 2025-05-22 22:36:06
+ */
+
+#pragma once
+#include "cppcommon/extends/abseil/absl.h"        // IWYU pragma: keep
+#include "cppcommon/extends/rapidjson/builder.h"  // IWYU pragma: keep
+#include "cppcommon/extends/rapidjson/reader.h"   // IWYU pragma: keep
+#include "cppcommon/extends/spdlog/log.h"         // IWYU pragma: keep
+#include "cppcommon/utils/common.h"               // IWYU pragma: keep
+#include "cppcommon/utils/error.h"                // IWYU pragma: keep
+#include "cppcommon/utils/macros.h"               // IWYU pragma: keep
+#include "cppcommon/utils/str.h"                  // IWYU pragma: keep
+#include "cppcommon/utils/to_str.h"               // IWYU pragma: keep
+#include "cppcommon/utils/type_traits.h"          // IWYU pragma: keep
+
+namespace cppcommon {}
---
+++ cppcommon/utils/macros.h
@@ -0,0 +1,11 @@
+/**
+ * @file macros.h
+ * @brief
+ * @author zhenkai.sun
+ * @date 2025-05-22 22:49:03
+ */
+#pragma once
+
+#ifndef Enum2I
+#define Enum2I(e) static_cast<int>(e)
+#endif
