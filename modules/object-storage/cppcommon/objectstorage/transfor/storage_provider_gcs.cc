f797aa0f2b417a5ee0ffe82b802b4842ee47d239
zhenkai.sun <zhenkai.sun@qq.com>
2025-06-04 23:54:44 +0800

    add gcs download

--- modules/object-storage/cppcommon/objectstorage/transfor/storage_provider_gcs.cc
+++ modules/object-storage/cppcommon/objectstorage/transfor/storage_provider_gcs.cc
@@ -13,6 +13,7 @@
 
 #include "absl/status/status.h"
 #include "cppcommon/objectstorage/transfor/storage_provider.h"
+#include "spdlog/spdlog.h"
 
 namespace cppcommon::os {
 GcsStorageProvider::GcsStorageProvider(StorageProviderOptions &&options) {
@@ -46,13 +47,60 @@ absl::Status GcsStorageProvider::Upload(const TransferMeta &m) {
   return absl::OkStatus();
 }
 
-absl::Status GcsStorageProvider::DownloadFile(const TransferMeta &meta) {
-  throw std::runtime_error("method not be implemented");
-  // return absl::OkStatus();
+absl::Status GcsStorageProvider::DownloadFile(const TransferMeta &m) {
+  auto &lp = m.local_file_path;
+  ExpectOrInternal(!lp.empty(), FMT("destination path should not be empty. [{}]", m.ToString()));
+  ExpectOrInternal(lp[lp.size() - 1] != '/', FMT("destination file path should not end with '/'. [{}]", m.ToString()));
+
+  // 1. ensure local dest path
+  OkOrRet(EnsureLocalPath(fs::path(m.local_file_path), m.overwrite));
+
+  // 2. download file
+  auto writer = client_->ReadObject(m.bucket, m.remote_file_path);
+  if (!writer) {
+    return absl::InternalError(FMT("Failed to read GCS object. [bucket={}, path={}]", m.bucket, m.remote_file_path));
+  }
+
+  std::ofstream outFile(m.local_file_path, std::ios::binary);
+  if (!outFile) {
+    return absl::InternalError(FMT("Failed to open local file. [path={}]", m.local_file_path));
+  }
+
+  outFile << writer.rdbuf();
+  if (!outFile) {
+    return absl::InternalError(FMT("Failed to write to local file. [path={}]", m.local_file_path));
+  }
+
+  return absl::OkStatus();
 }
 
 std::vector<std::filesystem::path> GcsStorageProvider::Download(const TransferMeta &meta) {
-  throw std::runtime_error("method not be implemented");
-  // return {};
+  std::filesystem::path base = std::filesystem::path(meta.local_file_path) / std::filesystem::path(meta.file_name);
+  std::vector<std::filesystem::path> result;
+  // list files
+  google::cloud::storage::ListObjectsReader reader =
+      client_->ListObjects(meta.bucket, google::cloud::storage::Prefix(meta.remote_file_path));
+
+  for (auto const &object : reader) {
+    if (!object) {
+      spdlog::error("Failed to list GCS objects. [err={}]", object.status().message());
+      continue;
+    }
+
+    auto key = object->name();
+    auto cur = base / std::filesystem::path(key.substr(meta.remote_file_path.size()));
+
+    if (key.back() == '/') continue;
+
+    auto dm = TransferMeta{.bucket = meta.bucket, .remote_file_path = key, .local_file_path = cur};
+    auto s = DownloadFile(dm);
+    if (!s.ok()) {
+      spdlog::error("Download gcs object failed. [info={}, error={}]", dm.ToString(), s.ToString());
+    } else {
+      spdlog::info("Download gcs object success. [info={}]", dm.ToString());
+    }
+    result.emplace_back(cur);
+  }
+  return result;
 }
 }  // namespace cppcommon::os
