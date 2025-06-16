f0c0afead4cf2ff567a3b2c06b54550eb553a555
zhenkai.sun <zhenkai.sun@qq.com>
2025-06-16 17:52:51 +0800

    add filter & ignore

--- tests/gtest/gtest_compare.cc
+++ tests/gtest/gtest_compare.cc
@@ -24,11 +24,20 @@ TEST(Cmp, JS) {
   // record 2
   json1 = R"({"a": 1, "b": [1, 2, 3], "c": {"d": "x"}, "f": "1,2,3"})";
   json2 = R"({"a": 2, "b": [1, 3], "c": {"d": "x"}, "e": 5, "f": "1,3,5"})";
-  s = cppcommon::rapidjson::DiffJson(stat, {.vector_options{.delimiter = ','}}, json1, json2);
+  s = cppcommon::rapidjson::DiffJson(stat, {.vector_options{.delimiter = ','}, .filter_pathes = {"a"}}, json1, json2);
   spdlog::info("{}: {}", s.ToString(), stat.ToString());
 }
 
 TEST(Cmp, JsV2) {
   cppcommon::rapidjson::BatchDiffResultStat stat;
   spdlog::info("{}", stat.ToString());
+}
+
+TEST(Cmp, JsV3) {
+  std::string json1 = R"({"a": 1, "b": [1, 2, 3], "c": {"d": "x"}, "f": "1,2,3"})";
+  std::string json2 = R"({"a": 2, "b": [1, 3], "c": {"d": "x"}, "e": 5, "f": "1,3,5"})";
+  cppcommon::rapidjson::BatchDiffResultStat stat;
+  auto s = cppcommon::rapidjson::DiffJson(stat, {.vector_options{.delimiter = ','}, .filter_pathes = {".*a", "/b.*"}},
+                                          json1, json2);
+  spdlog::info("{}: {}", s.ToString(), stat.ToString());
 }
