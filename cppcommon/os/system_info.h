2d2bb6ec588b8bb96bba389ee865a36f0513ab5c
zhenkai.sun <zhenkai.sun@qq.com>
2025-07-19 11:06:45 +0800

    add os systeminfog

---
+++ cppcommon/os/system_info.h
@@ -0,0 +1,11 @@
+/**
+ * @file system_info.h
+ * @brief
+ * @author zhenkai.sun
+ * @date 2025-07-19 10:50:16
+ */
+#pragma once
+
+namespace cppcommmon {
+struct SystemInfo {};
+}  // namespace cppcommmon
